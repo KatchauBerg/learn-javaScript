5f1bd001daa6d1f3d39d28b7cc67ddfb0713ff09
KatchauBerg <kevinlima138@gmail.com>
2024-12-10 17:58:15 -0300

    update archives

--- index.js
+++ index.js
@@ -1,34 +1,5 @@
-class Cliente {
-
-  nome
-  cpf
-}
-
-class ContaCorrente{
-
-  agencia
-  saldo
-
-  sacar(valor){
-
-    if(this.saldo >= valor){
-
-      this.saldo -= valor 
-    }
-
-    return valor
-  }
-
-  depositar(valor){
-
-    if(valor > 0){
-
-      this.saldo += valor 
-    }
-
-    return valor
-  }
-}
+import { Cliente } from "./Cliente.js"
+import { ContaCorrente } from "./ContaCorrente.js"
 
 const cliente1 = new Cliente()
 cliente1.nome = "Ricardo"
@@ -40,8 +11,6 @@ cliente2.cpf = 999999999999
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001
-contaCorrenteRicardo.saldo = 10
-
 let valorSacado = 200;
 contaCorrenteRicardo.sacar(valorSacado)
 contaCorrenteRicardo.depositar(100)
